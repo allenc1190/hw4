c9eb0b45207eccaa54d95402d831aeaf1158373c
allenc1190 <chanall@oregonstate.edu>
2020-11-17 05:21:05 +0000

    before ad

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 //Code from the example
 
 //creates buffer
-int size = 9999;
+int size = 1000;
 //Buffer 1
 char buffer_1[49][1000];
 // Number of items in the buffer
@@ -104,17 +104,21 @@ char* getUserInp(){
 	return inp2;
 }
 */
-//get user input for thread1
+
+//thread1
 void* input(void* args){
 	//printf("\nthread 1\n");
   //char *inp = getUserInp();
 	//put_buff_1(inp);
   //printf("%s\n", buffer_1);
+
+	
   size_t len;
   char *inp2;
   ssize_t nread;
-  while((nread = getline(&inp2, &len, stdin)) != -1)
-  {
+	//strcpy(buffer_1[prod_idx_1], inp2);
+	//read each line of file
+  while((nread = getline(&inp2, &len, stdin)) != -1){
     //printf("%s\n", inp2);
     pthread_mutex_lock(&mutex_1);
     strcpy(buffer_1[prod_idx_1], inp2);
@@ -122,58 +126,38 @@ void* input(void* args){
     prod_idx_1++;
     pthread_cond_signal(&full_1);
     pthread_mutex_unlock(&mutex_1);
-    if (strcmp(inp2, "STOP\n") == 0)
+    if(strcmp(inp2, "STOP\n") == 0)
       break;
   }
-  // for(int i = 0; i < prod_idx_1; i++)
-  // {
+  // for(int i = 0; i < prod_idx_1; i++){
   //   printf("%s\n", buffer_1[i]);
   // }
 	return NULL;
 }
 
-char *createLineSep(){
-	char *afReplace = get_buff_2();
-	//printf("\n\nBUFF 2 == %s\n\n", afReplace);
-	int i;
-	int len2 = strlen(afReplace);	
-	
-	char *temp2 = (char*)malloc(len2);
 
-	for(i = 0; i < len2; i++){
-		temp2[i] = afReplace[i];
-		
-		if(afReplace[i] == '\n'){
-			temp2[i] = ' ';
-		}
-		//printf("temppppp ======= %s\n", temp2);
-	}
-
-	//printf("TEMP2 == %s\n", temp2);
-
-	return temp2;
-}
-
+//thread 2
 void* lineSep(void* args){
 	//printf("thread 2\n");
 	//char *inp3 = createLineSep();
 	//printf("\n\nBline sep == %s\n\n", inp3);
-  for (int i = 0; i < 49; i++)
-  {
+  for (int i = 0; i < 49; i++){
     pthread_mutex_lock(&mutex_1);
-    while (count_1 == 0)
+    while(count_1 == 0){
       pthread_cond_wait(&full_1, &mutex_1);
+		}
 
     char *sec = malloc(1000 * sizeof(char));
     strcpy(sec, buffer_1[prod_idx_2]);
     count_1--;
     pthread_mutex_unlock(&mutex_1);
-    //printf("%s\n", sec);
+    //printf("HELLO === %s\n", sec);
     int ii = 0;
-    while(sec[ii] != '\n')
-    {
+		//replace all \n with space
+    while(sec[ii] != '\n'){
       ii++;
     }
+
     sec[ii] = ' ';
     pthread_mutex_lock(&mutex_2);
     strcpy(buffer_2[prod_idx_2], sec);
@@ -182,7 +166,7 @@ void* lineSep(void* args){
     pthread_cond_signal(&full_2);
     pthread_mutex_unlock(&mutex_2);
 
-    if (strcmp(sec, "STOP ")==0)
+    if(strcmp(sec, "STOP ") == 0)
       break;
 
     //printf("index: %d \n %s \n", prod_idx_2, buffer_2[prod_idx_2 - 1]);
@@ -228,25 +212,25 @@ char *replace(char* input){
 //get the new string after repalcing all ++ with ^
 char* afterPlus(){
 	char* finall;
-  finall = get_buff_1();
+  //finall = get_buff_1();
 	finall = replace(finall);
 	//put string with carets in buffer 2
-	put_buff_2(finall);
+	//put_buff_2(finall);
 	//printf("New string: %s\n", finall);
 	//printf("after plus: \n %s\n", finall);
 	return finall;
 }
 
-//thread 2
+//thread 3
 void* plusSign(void* args){
 	//printf("\nThread 3\n");
 	//char *aPlus = afterPlus();
 	//printf("here === %s\n", aPlus);
-  for (int i = 0; i < 49; i++)
-  {
+  for (int i = 0; i < 49; i++){
     pthread_mutex_lock(&mutex_2);
-    while (count_2 == 0)
+    while(count_2 == 0){
       pthread_cond_wait(&full_2, &mutex_2);
+		}
 
     char *sec = malloc(1000 * sizeof(char));
     strcpy(sec, buffer_2[prod_idx_3]);
@@ -254,23 +238,25 @@ void* plusSign(void* args){
     pthread_mutex_unlock(&mutex_2);
 
     char *sec2 = replace(sec);
+
     //printf("%s\n", sec2);
     //printf("%s\n", sec);
     pthread_mutex_lock(&mutex_3);
     strcpy(buffer_3[prod_idx_3], sec2);
+
     prod_idx_3++;
     count_3++;
     pthread_cond_signal(&full_3);
     pthread_mutex_unlock(&mutex_3);
 
-    if (strcmp(sec, "STOP ")==0)
+    if(strcmp(sec, "STOP ") == 0){
       break;
+		}
 
     //printf("index: %d \n %s \n", prod_idx_2, buffer_2[prod_idx_2 - 1]);
   }
 
-  for (int i = 0; i < prod_idx_3; i++)
-  {
+  for(int i = 0; i < prod_idx_3; i++){
     printf("%s\n", buffer_3[i]);
   }
 
@@ -288,7 +274,7 @@ char *createEighty(){
 	//allocates memory for temp
 	char *temp = (char*)malloc(num+num2);
 	//loop through each character until it reaches the last character
-  printf("here\n %s\n", oldstr);
+  //printf("here\n %s\n", oldstr);
 
 	for(int i = 0; i < num+num2; i++){
 		count++;
@@ -309,9 +295,23 @@ char *createEighty(){
 //thread 4
 void* output(void* args){
 	//printf("\nthread 4\n");
-	char *finalOut = createEighty();
-	printf("\n%s\n", finalOut);
+	//char *finalOut = createEighty();
+	//printf("\n%s\n", finalOut);
 	//printf("AFTER\n");
+
+	for(int i = 0; i < 49; i++){
+		pthread_mutex_lock(&mutex_3);
+		while(count_3 == 0){
+			pthread_cond_wait(&full_3, &mutex_3);
+		}
+
+		char *sec = malloc(1000 * sizeof(char));
+		
+	}
+
+	for(int i = 0; i < prod_idx_3; i++){
+		//printf("TEST %s\n", buffer_3[i]);
+	}
 	
 	return NULL;
 }
@@ -325,13 +325,13 @@ int main(){
 	pthread_create(&input_t, NULL, input, NULL);
 	pthread_create(&lineSep_t, NULL, lineSep, NULL);
 	pthread_create(&plusSign_t, NULL, plusSign, NULL);
-	pthread_create(&output_t, NULL, output, NULL);
+	//pthread_create(&output_t, NULL, output, NULL);
 
 
 	pthread_join(input_t, NULL);
 	pthread_join(lineSep_t, NULL);
 	pthread_join(plusSign_t, NULL);
-	pthread_join(output_t, NULL);
+	//pthread_join(output_t, NULL);
 
 	//printf("After thread\n");
 
